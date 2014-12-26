cc1b08f67f5e551017ca8d28e5bda8c19aa10554
r8921025git <r8921025+git@yahoo.com.tw>
2014-12-25 23:01:14 -0500

    now it works correctly

--- Swap bits.cpp
+++ Swap bits.cpp
@@ -2,24 +2,58 @@
 
 using namespace std;
 
+template<class T>
+inline int numbits(T num)
+{
+  return sizeof(num)*8;
+}
+
+template<class T>
+inline void printBits(T num) {
+  int nbits = numbits(num);
+  unsigned long long mask = 1;
+  mask = mask << (nbits-1);
+  for(int i=0;i<nbits;++i) {
+    if(i%4==0)
+      std::cout << " ";
+    std::cout << (num & mask ? 1 : 0);
+    mask = mask >> 1;
+  }
+  std::cout << std::endl;
+}
+
+
+//swap bit number i and bit number j
 long SwapBits(long x, int i, int j) {
   cout<<"x="<<x<<endl;
-  long a = 1&&(x>>i);
-  long b = 1&&(x>>j);
+  long a = 1&(x>>i);
+  long b = 1&(x>>j);
   cout<<"a="<<a<<endl;
   cout<<"b="<<b<<endl;
-  x = x && a<<j;
-  x = x && b<<i;
-
+  // if both a,b are 0, then do nothing
+  // if both a,b are 1, then do nothing
+  
+  if (a==b)
+    return x;
+  else {
+    // if a==0,b==1 or a==1,b==0, then flip the bit with XOR(^1)
+    x = x ^ 1<<i;
+    x = x ^ 1<<j;
+  }
   return x;
 }
 
 
 int main()
 {
-   cout << "Hello World" << endl; 
-   long out = SwapBits(4,0,1);
+   
+   long x=11;
+   cout<<"input="<<x<<endl;;
+   printBits(x);
+   
+   long out = SwapBits(x,4,0);
    cout<<"out="<<out<<endl;
+   printBits(out);
    
    return 0;
 }
